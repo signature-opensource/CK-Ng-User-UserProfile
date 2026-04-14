4b768d5ee6bfe2662e211b75b2215e232a1eae9b
Arthur Cheng <arthur.cheng@signature.one>
2026-04-14 12:45:23 +0200

    added ICommand typescript type registrations and fixed user's resolved defaultCulture

--- CK.Ng.UserProfile.PreferredCulture/UserProfilePreferredCulturePackage.cs
+++ CK.Ng.UserProfile.PreferredCulture/UserProfilePreferredCulturePackage.cs
@@ -1,10 +1,12 @@
 using CK.Core;
+using CK.IO.User.PreferredCulture;
 using CK.TypeScript;
 
 namespace CK.Ng.UserProfile.PreferredCulture;
 
 [TypeScriptPackage]
 [Requires<UserProfilePackage>]
+[RegisterTypeScriptType( typeof( ISetUserPreferredCultureCommand ) )]
 public class UserProfilePreferredCulturePackage : TypeScriptPackage
 {
 }
