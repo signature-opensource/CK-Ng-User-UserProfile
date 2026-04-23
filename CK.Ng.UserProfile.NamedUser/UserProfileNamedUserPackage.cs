9671adbb4a31906f10c49b5859d978da07b416db
Arthur Cheng <arthur.cheng@signature.one>
2026-04-23 17:18:37 +0200

    added RegisterTypeScriptType to auto register commands

--- CK.Ng.UserProfile.NamedUser/UserProfileNamedUserPackage.cs
+++ CK.Ng.UserProfile.NamedUser/UserProfileNamedUserPackage.cs
@@ -1,12 +1,12 @@
 using CK.Core;
-using CK.IO.Actor;
+using CK.IO.User.NamedUser;
 using CK.TypeScript;
 
 namespace CK.Ng.UserProfile.NamedUser;
 
 [TypeScriptPackage]
 [Requires<UserProfilePackage>]
-[RegisterTypeScriptType( typeof( ISetUserNameCommand ) )]
+[RegisterTypeScriptType( typeof( ISetUserNamesCommand ) )]
 public class UserProfileNamedUserPackage : TypeScriptPackage
 {
 }
